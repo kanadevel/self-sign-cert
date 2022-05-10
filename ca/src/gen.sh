46d208f076aeeb49914e58bf6d3d4c772d1682c3
kanadevel <50098708+kanadevel@users.noreply.github.co>
2022-05-10 17:23:51 +0900

    replace expiration date for chrome

--- ca/src/gen.sh
+++ ca/src/gen.sh
@@ -6,7 +6,12 @@ echo "ca name: $CANAME"
 openssl genrsa 2048 > /out/ca.key
 
 # Generate certificate sigining requests from same key.
-openssl req -new -key /out/ca.key -subj "/C=AU/ST=Some-State/O=Self Sign Cert CA/CN=${CANAME}" > /out/ca.csr
+openssl req -new -key /out/ca.key -subj "/C=AU/ST=Some-State/O=${CANAME}/CN=${CANAME}" > /out/ca.csr
 
 # Generate CA certificate.
 openssl ca -batch -extensions v3_ca -out /out/ca.crt -in /out/ca.csr -selfsign -keyfile /out/ca.key
+
+
+openssl x509 -in /out/ca.crt -out /out/tmp.der -outform DER
+openssl x509 -in /out/tmp.der -inform DER -out /out/ca.pem -outform pem
+rm /out/tmp.der
--- server/src/gen.sh
+++ server/src/gen.sh
@@ -9,4 +9,11 @@ envsubst < subjectnames-template.txt > subjectnames.txt
 openssl req -new -key /ca/ca.key -subj "/C=AU/ST=Some-State/O=Self Sign Cert Server/CN=${HOST}" > /out/server.csr
 
 # Generate server certificate with CA certificate.
-openssl x509 -days 3650 -req -extfile subjectnames.txt -CA /ca/ca.crt -CAkey /ca/ca.key -set_serial 1 < /out/server.csr > /out/server.crt
+openssl x509 -days 365 -req -extfile subjectnames.txt -CA /ca/ca.crt -CAkey /ca/ca.key -set_serial 1 < /out/server.csr > /out/server.crt
+
+
+openssl x509 -in /out/server.crt -out /out/tmp.der -outform DER
+openssl x509 -in /out/tmp.der -inform DER -out /out/server.pem -outform pem
+rm /out/tmp.der
+
+cp -p /ca/ca.key /out/server.key
